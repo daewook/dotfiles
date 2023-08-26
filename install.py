be2c0eb61fc3b6d55e574718d7b2da79c6e82098
daewook <daewook.kim11@gmail.com>
2023-08-26 16:46:36 -0400

    backup and overwrite dotfiles when they exist

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
 # chatgpt4 wrote most this for me :)
 
-import os
+import os, shutil
 
 def create_symlinks(folder_path):
     # List of files for which to create symlinks
@@ -14,6 +14,18 @@ def create_symlinks(folder_path):
         # Construct the source and destination paths
         source_path = os.path.join(folder_path, filename)
         destination_path = os.path.join(home_dir, '.' + filename[1:])
+
+        # If the destination file already exists, back it up and remove it
+        if os.path.exists(destination_path):
+            # Construct the backup file path
+            backup_path = destination_path + '.bak'
+
+            # Create a backup
+            shutil.copy2(destination_path, backup_path)
+            print(f"Created backup: {backup_path}")
+
+            # Remove the original file
+            os.remove(destination_path)
 
         # Create the symbolic link
         os.symlink(source_path, destination_path)
